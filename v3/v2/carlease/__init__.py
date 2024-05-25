45539398c037cd782605325393fc85fcf3467823
SachLawliet <135232726+SachLawliet@users.noreply.github.com>
2024-05-25 17:10:47 +0200

    Add files via upload

---
+++ v3/v2/carlease/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_postgresql import PostgreSQL
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a86fe86d8f95fe0cda48f9efb34c0424'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:sadcar@localhost/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+bootstrap = Bootstrap(app)
+
+#RESET PW BEGIN
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'carlease4@gmail.com'
+app.config['MAIL_PASSWORD'] = 'bzgxeevhiswmoers'
+mail = Mail(app)
+
+#END RESET PW
+
+
+from carlease import routes
+
