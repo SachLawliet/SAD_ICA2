1100d300fef3706da180d621645ed98a78121fa4
SachLawliet <135232726+SachLawliet@users.noreply.github.com>
2024-05-26 22:35:56 +0200

    Add files via upload

---
+++ v2/carlease/__init__.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_postgresql import PostgreSQL
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a86fe86d8f95fe0cda48f9efb34c0424'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:sadcar@localhost/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+bootstrap = Bootstrap(app)
+
+#RESET PW BEGIN
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'carlease4@gmail.com'
+app.config['MAIL_PASSWORD'] = 'ofcdwnzvcqspdnls'
+mail = Mail(app)
+
+#END RESET PW
+
+SITE_KEY = '6Le6uOgpAAAAAOB8UclqUV0G9_1WI1ovxNDFVm-C'
+SECRET_KEY = '6Le6uOgpAAAAAGSqKxIU5VkwB7rUmHpoe6gvmrpX'
+VERIFY_URL= 'https://www.google.com/recaptcha/api/siteverify'
+
+from carlease import routes
+
+
+with app.app_context():
+    db.create_all()
+    pass
